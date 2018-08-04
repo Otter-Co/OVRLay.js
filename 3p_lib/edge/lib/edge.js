0021cbe5548b88c02287a070af1a8a86c76c7b04
Ben Otter <benotter@me.com>
2018-08-04 01:45:55 -0700

    More 3P mods

--- 3p_lib/edge/lib/edge.js
+++ 3p_lib/edge/lib/edge.js
@@ -1,30 +1,42 @@
-const archPath = ( (process.arch === "x64")
-    ? "./native/win32/x64"
-    : "./native/win32/ia32"
+const path = require('path');
+
+const archPath = ((process.arch === "x64")
+    ? __dirname + path.sep + ["native", "win32", "x64", "8.2.1", "edge_nativeclr.node"].join(path.sep)
+    : __dirname + path.sep + ["native", "win32", "ia32", "8.2.1", "edge_nativeclr.node"].join(path.sep)
 );
+
+console.log(archPath);
 
 process.env.EDGE_NATIVE = archPath;
 const edge = require(archPath);
 
-exports.func = function(language, options) {
-    if (!options) {
+exports.func = function (language, options)
+{
+    if (!options)
+    {
         options = language;
         language = 'cs';
     }
 
-    if (typeof options === 'string') {
-        if (options.match(/\.dll$/i)) {
+    if (typeof options === 'string')
+    {
+        if (options.match(/\.dll$/i))
+        {
             options = { assemblyFile: options };
         }
-        else {
+        else
+        {
             options = { source: options };
         }
     }
-    else if (typeof options === 'function') {
+    else if (typeof options === 'function')
+    {
         var originalPrepareStackTrace = Error.prepareStackTrace;
         var stack;
-        try {
-            Error.prepareStackTrace = function(error, stack) {
+        try
+        {
+            Error.prepareStackTrace = function (error, stack)
+            {
                 return stack;
             };
             stack = new Error().stack;
@@ -33,79 +45,100 @@ exports.func = function(language, options) {
         {
             Error.prepareStackTrace = originalPrepareStackTrace;
         }
-        
+
         options = { source: options, jsFileName: stack[1].getFileName(), jsLineNumber: stack[1].getLineNumber() };
     }
-    else if (typeof options !== 'object') {
+    else if (typeof options !== 'object')
+    {
         throw new Error('Specify the source code as string or provide an options object.');
     }
 
-    if (typeof language !== 'string') {
+    if (typeof language !== 'string')
+    {
         throw new Error('The first argument must be a string identifying the language compiler to use.');
     }
-    else if (!options.assemblyFile) {
+    else if (!options.assemblyFile)
+    {
         var compilerName = 'edge-' + language.toLowerCase();
         var compiler;
-        try {
+        try
+        {
             compiler = require(compilerName);
         }
-        catch (e) {
+        catch (e)
+        {
             throw new Error("Unsupported language '" + language + "'. To compile script in language '" + language +
                 "' an npm module '" + compilerName + "' must be installed.");
         }
 
-        try {
+        try
+        {
             options.compiler = compiler.getCompiler();
         }
-        catch (e) {
+        catch (e)
+        {
             throw new Error("The '" + compilerName + "' module required to compile the '" + language + "' language " +
                 "does not contain getCompiler() function.");
         }
 
-        if (typeof options.compiler !== 'string') {
+        if (typeof options.compiler !== 'string')
+        {
             throw new Error("The '" + compilerName + "' module required to compile the '" + language + "' language " +
                 "did not specify correct compiler package name or assembly.");
         }
 
-        if (process.env.EDGE_USE_CORECLR) {
+        if (process.env.EDGE_USE_CORECLR)
+        {
             options.bootstrapDependencyManifest = compiler.getBootstrapDependencyManifest();
         }
     }
 
-    if (!options.assemblyFile && !options.source) {
-        throw new Error('Provide DLL or source file name or .NET script literal as a string parmeter, or specify an options object '+
+    if (!options.assemblyFile && !options.source)
+    {
+        throw new Error('Provide DLL or source file name or .NET script literal as a string parmeter, or specify an options object ' +
             'with assemblyFile or source string property.');
     }
-    else if (options.assemblyFile && options.source) {
+    else if (options.assemblyFile && options.source)
+    {
         throw new Error('Provide either an asseblyFile or source property, but not both.');
     }
 
-    if (typeof options.source === 'function') {
+    if (typeof options.source === 'function')
+    {
         var match = options.source.toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/);
-        if (match) {
+        if (match)
+        {
             options.source = match[1];
         }
-        else {
+        else
+        {
             throw new Error('If .NET source is provided as JavaScript function, function body must be a /* ... */ comment.');
         }
     }
 
-    if (options.references !== undefined) {
-        if (!Array.isArray(options.references)) {
+    if (options.references !== undefined)
+    {
+        if (!Array.isArray(options.references))
+        {
             throw new Error('The references property must be an array of strings.');
         }
 
-        options.references.forEach(function (ref) {
-            if (typeof ref !== 'string') {
+        options.references.forEach(function (ref)
+        {
+            if (typeof ref !== 'string')
+            {
                 throw new Error('The references property must be an array of strings.');
             }
         });
     }
 
-    if (options.assemblyFile) {
-        if (!options.typeName) {
+    if (options.assemblyFile)
+    {
+        if (!options.typeName)
+        {
             var matched = options.assemblyFile.match(/([^\\\/]+)\.dll$/i);
-            if (!matched) {
+            if (!matched)
+            {
                 throw new Error('Unable to determine the namespace name based on assembly file name. ' +
                     'Specify typeName parameter as a namespace qualified CLR type name of the application class.');
             }
@@ -113,11 +146,13 @@ exports.func = function(language, options) {
             options.typeName = matched[1] + '.Startup';
         }
     }
-    else if (!options.typeName) {
+    else if (!options.typeName)
+    {
         options.typeName = "Startup";
     }
 
-    if (!options.methodName) {
+    if (!options.methodName)
+    {
         options.methodName = 'Invoke';
     }
 
